ef4518a0f096890ce5ebc47e7f7c471ba8d54079
kaweeweewee <135637586+kaweeweewee@users.noreply.github.com>
2026-08-28 18:08:54 +0700

    Add files via upload

---
+++ ```javascript.js
@@ -0,0 +1,12 @@
+```javascript
+// แสดงข้อความใน Console เมื่อเปิดเว็บไซต์
+console.log("BasketWiki เริ่มทำงานแล้ว!");
+
+
+// เมื่อกดปุ่ม "เริ่มสำรวจ"
+const button = document.querySelector(".button");
+
+button.addEventListener("click", function () {
+    console.log("กำลังเข้าสู่ข้อมูลนักบาส");
+});
+```
